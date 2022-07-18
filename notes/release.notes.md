6332d7693b5566510d82a274932c04e931946918
Gildas Rouault <g.rouault@groupe-pomona.fr>
2022-07-18 22:51:33 +0200

    transaction : par programmation avec l'API du gestionnaire de Transaction

--- notes/release.notes.md
+++ notes/release.notes.md
@@ -1,8 +1,52 @@
 ## spring.jdbc
 
-### configuration
-- mysql : v8
-- gestion avec flywaydb
+### transaction : par programmation avec l'API du gestionnaire de Transaction
+- mise en place de JdbcTemplate et de JdbcDaoSupport
+  - plus de gestion de la connexion
+  - plus de SqlException
+- Transaction : gérer manuellement avec le gestionnaire de transaction
+- IMPORTANT : mise en place du logger sur
+  <Logger name="org.springframework.jdbc" level="debug" />
+  <Logger name="org.springframework.jdbc.datasource.DataSourceTransactionManager" level="debug" />
+
+### transaction : jdbc avec mise en place du rollback
+- désactivation de la gestion automatique des transactions
+- mise en place du commit / rollback
+
+### transaction : initialisation
+- mise en place projet BookShop
+- ajout d'une nouvelle base de données
+- main sans transaction : en mode autocommit
+  - dans le main, une exception est levée et pas de rollback opéré
+  
+### exception : ajout test insertion en erreur
+- test en erreur
+- main : exécution en erreur
+- dataSource: configuration Pool
+
+### spring - NamedParameterJdbcDaoSupport
+- chaque dao hérite de la classe Spring : NamedParameterJdbcDaoSupport
+- utilisation de getNamedParameterJdbcTemplate() pour les paramètres nommés
+
+### spring - jdbcDaoSupport et getJdbcTemplate
+- injection de la dataSource dans les DAOs
+- chaque dao hérite de la classe Spring : jdbcDaoSupport
+- utilisation de la classe getJdbcTemplate()
+
+### spring - défnition jdbcTemplate
+- injection jdbcTemplate dans le DAO
+
+### requete Select - Update
+- mise en place des requetes update
+- mise en place des requetes select
+
+### mise en place spring + ordre des tests
+- mise en place de Spring : (data-context.xml / database.properties)
+  - configuration dataSource
+  - injection dataSource dans le DAO Véhicule
+- finalisation des tests
+  - mise en place de l'ordre des tests
+  - truncate sur véhicule
 
 ### version initiale
 - pas de spring
@@ -13,42 +57,15 @@
   - L'implémenteur track les SQLException (Erreur spécifique JDBC)
     et remonte une simple RuntimeException
 
+### configuration
+- mysql : v8
+- gestion avec flywaydb
 
-### mise en place spring + ordre des tests
-- mise en place de Spring : (data-context.xml / database.properties)
-  - configuration dataSource
-  - injection dataSource dans le DAO Véhicule
-- finalisation des tests
-  - mise en place de l'ordre des tests
-  - truncate sur véhicule
 
-### requete Select - Update
-- mise en place des requetes update 
-- mise en place des requetes select
 
-### spring - défnition jdbcTemplate
-- injection jdbcTemplate dans le DAO
 
-### spring - jdbcDaoSupport et getJdbcTemplate
-- injection de la dataSource dans les DAOs
-- chaque dao hérite de la classe Spring : jdbcDaoSupport
-- utilisation de la classe getJdbcTemplate()
 
-### spring - NamedParameterJdbcDaoSupport
-- chaque dao hérite de la classe Spring : NamedParameterJdbcDaoSupport
-- utilisation de getNamedParameterJdbcTemplate() pour les paramètres nommés
 
-### exception : ajout test insertion en erreur
-- test en erreur 
-- main : exécution en erreur
-- dataSource: configuration Pool
 
-### transaction : initialisation
-- mise en place projet BookShop 
-- ajout d'une nouvelle base de données
-- main sans transaction : en mode autocommit
-  - dans le main, une exception est levée et pas de rollback opéré
 
-### transaction : jdbc avec mise en place du rollback
-- désactivation de la gestion automatique des transactions
-- mise en place du commit / rollback
+
--- src/main/java/com/exo/dao/impl/BookShopJdbcDAO.java
+++ src/main/java/com/exo/dao/impl/BookShopJdbcDAO.java
@@ -5,70 +5,67 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 import javax.sql.DataSource;
 import javax.xml.crypto.Data;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-public class BookShopJdbcDAO implements BookShopDAO {
+public class BookShopJdbcDAO extends NamedParameterJdbcDaoSupport implements BookShopDAO {
 
     private static final Logger LOG = LogManager.getLogger();
 
     @Autowired
-    private DataSource dataSource;
+    private PlatformTransactionManager transactionManager;
 
     @Override
     public void purchase(String isbn, String username) {
 
-        Connection conn = null;
+        String sql = null;
+        Map<String, Object> parameters = null;
+
+        // parametrage de la transaction.
+        TransactionDefinition def = new DefaultTransactionDefinition();
+        TransactionStatus status = transactionManager.getTransaction(def);
+
         try {
 
-            conn = dataSource.getConnection();
-            conn.setAutoCommit(false);
-
             // recuperation du prix du livre
-            PreparedStatement stmt1 = conn.prepareStatement("SELECT PRICE FROM BOOK WHERE ISBN = ?");
-            stmt1.setString(1, isbn);
-            ResultSet rs = stmt1.executeQuery();
-            rs.next();
-            int price = rs.getInt("PRICE");
-            stmt1.close();
+            sql = "SELECT PRICE FROM BOOK WHERE ISBN = :isbn";
+            parameters = new HashMap<>();
+            parameters.put("isbn", isbn);
+            int price = getNamedParameterJdbcTemplate().queryForObject(sql, parameters, Integer.class);
 
             // mise à jour du stock du livre.
-            PreparedStatement stmt2 = conn.prepareStatement("UPDATE BOOK_STOCK SET STOCK = STOCK - 1 WHERE ISBN = ?");
-            stmt2.setString(1, isbn);
-            stmt2.executeUpdate();
-            stmt2.close();
+            sql = "UPDATE BOOK_STOCK SET STOCK = STOCK - 1 WHERE ISBN = :isbn";
+            getNamedParameterJdbcTemplate().update(sql, parameters);
 
             // ajustement du solde du compte.
-            PreparedStatement stmt3 = conn.prepareStatement("UPDATE ACCOUNT SET BALANCE = BALANCE - ? WHERE USERNAME = ?");
-            stmt3.setInt(1, price);
-            stmt3.setString(2, username);
-            stmt3.executeUpdate();
-            stmt3.close();
+            sql = "UPDATE ACCOUNT SET BALANCE = BALANCE - :price WHERE USERNAME = :username";
+            parameters = new HashMap<>();
+            parameters.put("price", price);
+            parameters.put("username", username);
+            getNamedParameterJdbcTemplate().update(sql, parameters);
 
-            conn.commit();
+            transactionManager.commit(status);
 
-        } catch (SQLException e) {
-            if (conn != null) {
-                try {
-                    conn.rollback();
-                } catch (SQLException ex) {}
-            }
-            throw new RuntimeException(e);
-        } finally {
-            if (conn != null) {
-                try {
-                    conn.close();
-                } catch (SQLException e) {}
-            }
+        } catch (DataAccessException ex) {
+            transactionManager.rollback(status);
+            throw ex;
         }
 
     }
 
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
     }
 
 }
